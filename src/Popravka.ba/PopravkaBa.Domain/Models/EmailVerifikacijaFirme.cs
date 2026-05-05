060ba227f367d6aaca74d3018ad289e909e23d5f
astaffz <mustaficaid@gmail.com>
2026-05-05 11:25:29 +0200

    Dodaje EmailVerifikacijuFirme

---
+++ src/Popravka.ba/PopravkaBa.Domain/Models/EmailVerifikacijaFirme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopravkaBa.Domain.Models
+{
+
+    public class EmailVerifikacijaFirme : VerifikacijaFirme
+    {
+        public string AdminEmail { get; set; }
+
+        public override void ObradiVerifikaciju(bool odobri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PodnesiVerifikaciju()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
