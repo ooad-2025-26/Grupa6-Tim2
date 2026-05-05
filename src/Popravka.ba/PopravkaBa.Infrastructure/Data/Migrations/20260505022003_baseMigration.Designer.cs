7d0e6615c90c57d5462388ec89c5acdd2366518b
astaffz <mustaficaid@gmail.com>
2026-05-05 04:43:19 +0200

    feat: Migracija osnovnih entityeva

--- src/Popravka.ba/PopravkaBa.Infrastructure/Data/Migrations/20260505022003_baseMigration.Designer.cs
+++ src/Popravka.ba/PopravkaBa.Infrastructure/Data/Migrations/20260505022003_baseMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Popravka.ba.Data;
 
@@ -11,9 +12,11 @@ using Popravka.ba.Data;
 namespace Popravka.ba.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260505022003_baseMigration")]
+    partial class baseMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Popravka.ba/PopravkaBa.Infrastructure/Data/Migrations/20260505022003_baseMigration.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Popravka.ba.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class baseMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "adresa",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "brojZavrsenihPoslova",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ime",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "maxZaposlenih",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "minZaposlenih",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "nazivFirme",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "opis",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "prezime",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "prosjecnaOcjena",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "slika",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "stambeniBroj",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "statusVerifikacije",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Kategorija",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategorija", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kategorija");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "adresa",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "brojZavrsenihPoslova",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ime",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "maxZaposlenih",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "minZaposlenih",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "nazivFirme",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "opis",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "prezime",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "prosjecnaOcjena",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "slika",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "stambeniBroj",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "statusVerifikacije",
+                table: "AspNetUsers");
+        }
+    }
+}
