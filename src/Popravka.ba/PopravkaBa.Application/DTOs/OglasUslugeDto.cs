b6df4ed25ecf13f9558bf7e76953657fa83844c1
TarikRedzic <tarik.redzic2005@gmail.com>
2026-05-09 16:12:45 +0200

    Kontroleri oglasa izrađeni, konačno

---
+++ src/Popravka.ba/PopravkaBa.Application/DTOs/OglasUslugeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopravkaBa.Application.DTOs
+{
+    public class ObjaviOglasUslugeDto
+    {
+    }
+
+    public class UrediOglasUslugeDto
+    {
+    }
+}
--- src/Popravka.ba/PopravkaBa.Web/Controllers/OglasController.cs
+++ src/Popravka.ba/PopravkaBa.Web/Controllers/OglasController.cs
@@ -108,4 +108,109 @@ namespace PopravkaBa.Web.Controllers
             return RedirectToAction(nameof(Index));
         }
     }
+
+    public class OglasRadnoMjestoController : Controller
+    {
+        private readonly IOglasRadnoMjestoService _oglasRadnoMjestoService;
+        private readonly ILogger<OglasRadnoMjestoController> _logger;
+
+        public OglasRadnoMjestoController(IOglasRadnoMjestoService oglasRadnoMjestoService, ILogger<OglasRadnoMjestoController> logger)
+        {
+            _oglasRadnoMjestoService = oglasRadnoMjestoService;
+            _logger = logger;
+        }
+
+
+        public async Task<IActionResult> Index(string? pretraga)
+        {
+            var oglasi = string.IsNullOrWhiteSpace(pretraga)
+                ? await _oglasRadnoMjestoService.DajSveOglase()
+                : await _oglasRadnoMjestoService.PronadjiOglase(pretraga);
+            ViewBag.Search = pretraga;
+            return View(oglasi);
+        }
+
+        public async Task<IActionResult> Detalji(int id)
+        {
+            var oglas = _oglasRadnoMjestoService.DajOglasPoId(id);
+            if (oglas is null) return NotFound();
+            return View(oglas);
+        }
+
+        public IActionResult KreirajOglas() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> KreirajOglas(KreirajOglasRadnoMjestoDto dto)
+        {
+            if (!ModelState.IsValid) return View(dto);
+
+            try
+            {
+                await _oglasRadnoMjestoService.KreirajOglas(dto);
+                TempData["Success"] = "Oglas je uspješno kreiran";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View(dto);
+            }
+        }
+
+        public async Task<IActionResult> UrediOglas(int id)
+        {
+            var oglas = await _oglasRadnoMjestoService.DajOglasPoId(id);
+            if (oglas is null) return NotFound();
+
+            var dto = new UrediOglasRadnoMjestoDto
+            { };
+
+            return View(dto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UrediOglas(int id, UrediOglasRadnoMjestoDto dto)
+        {
+            if (id != dto.id) return BadRequest();
+            if (!ModelState.IsValid) return View(dto);
+
+            try
+            {
+                await _oglasRadnoMjestoService.UrediOglas(dto);
+                TempData["Success"] = "Oglas je uspješno uređen";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+                throw;
+            }
+        }
+
+        public async Task<IActionResult> ObrisiOglas(int id)
+        {
+            var oglas = await _oglasRadnoMjestoService.DajKnjiguPoId(id);
+            if (oglas is null) return NotFound();
+            return View(oglas);
+        }
+
+        // POST /Books/Delete/5
+        [HttpPost, ActionName("Brisanje oglasa")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PotvrdaBrisanjaOglasa(int id)
+        {
+            try
+            {
+                await _oglasRadnoMjestoService.ObrisiOglas(id);
+                TempData["Success"] = "Oglas je obrisan.";
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
