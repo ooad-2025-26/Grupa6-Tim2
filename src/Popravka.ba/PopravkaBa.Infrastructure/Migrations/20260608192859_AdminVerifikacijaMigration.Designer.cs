e144863c6423a414f14239624d60955c89db6b1a
rijad-kasapovic <rkasapovic1@etf.unsa.ba>
2026-06-08 21:41:06 +0200

    added AdminVerifikacija migration, app can now start

--- src/Popravka.ba/PopravkaBa.Infrastructure/Migrations/20260608192859_AdminVerifikacijaMigration.Designer.cs
+++ src/Popravka.ba/PopravkaBa.Infrastructure/Migrations/20260608192859_AdminVerifikacijaMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Popravka.ba.Data;
@@ -11,9 +12,11 @@ using Popravka.ba.Data;
 namespace PopravkaBa.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260608192859_AdminVerifikacijaMigration")]
+    partial class AdminVerifikacijaMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Popravka.ba/PopravkaBa.Infrastructure/Migrations/20260608192859_AdminVerifikacijaMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PopravkaBa.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminVerifikacijaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+    name: "AdminVerificirao",
+    table: "AspNetUsers",
+    type: "boolean",
+    nullable: false,
+    defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+    name: "AdminVerificirao",
+    table: "AspNetUsers");
+        }
+    }
+}
